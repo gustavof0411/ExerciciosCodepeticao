fefed2665cbbf3f8a311cfebc9d47d60c6fbc769
Gustavo <gustavofr0411@gmail.com>
2023-07-31 16:11:08 -0300

    Backup antes de redefinir o método quemJoga()

--- jogoDaVelha.py
+++ jogoDaVelha.py
@@ -1,12 +1,27 @@
 def jogoDaVelha():
-    # Usuário decidindo se vai jogar contra um jogador ou contra a máquina
     print("Jogo da Velha\n")
 
     print("Cada jogador escolherá a posição que quer colocar sua jogada.")
     print("O jogador 1 jogará como \"X\" e o jogador 2 jogará como \"O\".")
     print("Boa sorte!")
 
+    # Método para criar um tabuleiro nxn
+    def criaTabuleiro(dimensaoTabuleiro):
+        tabuleiro = []
+        linhaTab = []
+        colunaTab = list(range(1, (dimensaoTabuleiro*dimensaoTabuleiro)+1, 1))
+        contadorAdicoes = 0
+        adicionador = 1
+        while (contadorAdicoes < dimensaoTabuleiro):
+            linhaTab = list(
+                range(adicionador, adicionador+dimensaoTabuleiro, 1))
+            tabuleiro.append(linhaTab)
+            adicionador += dimensaoTabuleiro
+            contadorAdicoes += 1
+        return tabuleiro
+
     # Método para exibir o tabuleiro
+
     def imprimeTabuleiro(tabuleiro):
         contadorLinhasSeparadoras = 0
         for linha in tabuleiro:
@@ -30,18 +45,20 @@ def jogoDaVelha():
             print()
             msg = "Digite a posição em que deseja inserir:"
             posicao = int(input(msg))
-            while (1 <= posicao <= 9):
-                linha = (posicao - 1) // 3
-                coluna = (posicao - 1) % 3
-                if (tabuleiro[linha][coluna] == simboloJog1 or tabuleiro[linha][coluna] == simboloJog2):
-                    print(
-                        "Esta posição já foi escolhida! Escolha outra posição.")
-                    quemJoga(jogadorDaVez, jog1, jog2, simboloJogDaVez,
-                             simboloJog1, simboloJog2, tabuleiro)
-                elif(tabuleiro):
-                    tabuleiro[linha][coluna] = simboloJogDaVez
-                else:
-                    print("Insira um número que pertença as posições disponíveis!")
+            while (not (1 <= posicao <= 9)):
+                print("Insira um número que pertença às posições disponíveis!")
+                posicao = int(input(msg))
+
+            for linha in range(len(tabuleiro)):
+                for coluna in range(linha):
+                    if (coluna == simboloJog1 or coluna == simboloJog2):
+                        print(
+                            "Esta posição já foi escolhida! Escolha outra posição.")
+                        quemJoga(jogadorDaVez, jog1, jog2, simboloJogDaVez,
+                                 simboloJog1, simboloJog2, tabuleiro)
+                    elif (tabuleiro):
+                        coluna = simboloJogDaVez
+
             contagemDeJogadas += 1
             jogadorDaVez = jog2
             simboloJogDaVez = simboloJog2
@@ -51,9 +68,13 @@ def jogoDaVelha():
             print()
             msg = "Digite a posição em que deseja inserir:"
             posicao = int(input(msg))
-            for linha in range(len(tabuleiro)):
-                for coluna in range(len(tabuleiro[linha])):
-                    if posicao == tabuleiro[linha][coluna]:
+            while (not (1 <= posicao <= 9)):
+                print("Insira um número que pertença as posições disponíveis!")
+                posicao = int(input(msg))
+
+            for linha in tabuleiro:
+                for coluna in range(len(linha)):
+                    if posicao == linha[coluna]:
                         if (tabuleiro[linha][coluna] == simboloJog1 or tabuleiro[linha][coluna] == simboloJog2):
                             print(
                                 "Esta posição já foi escolhida! Escolha outra posição.")
@@ -81,6 +102,3 @@ def jogoDaVelha():
 
     imprimeTabuleiro(tabuleiro)
     print("Fim de Jogo!")
-
-    #    else:
-    #        print("Você escolheu jogar contra o computador.")
